b34cd3020529bb86798ed5cc66d45d70dd6c7380
denis-luttcev <denis.luttcev@gmail.com>
2021-05-26 15:23:35 +0300

    Upgrade routes data source test

--- shared/src/androidTest/kotlin/ru/z8/louttsev/cheaptripmobile/shared/RouteRepositoryTest.kt
+++ shared/src/androidTest/kotlin/ru/z8/louttsev/cheaptripmobile/shared/RouteRepositoryTest.kt
@@ -5,6 +5,8 @@ import android.support.test.runner.AndroidJUnit4
 import androidx.test.core.app.ApplicationProvider
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import ru.z8.louttsev.cheaptripmobile.shared.infrastructure.datasource.FullDbDataSource
+import ru.z8.louttsev.cheaptripmobile.shared.infrastructure.datasource.RouteDataSourceFullDb
 import ru.z8.louttsev.cheaptripmobile.shared.infrastructure.persistence.LocalDb
 import ru.z8.louttsev.cheaptripmobile.shared.model.DataSource
 import ru.z8.louttsev.cheaptripmobile.shared.model.DataSource.ParamsBundle
@@ -15,6 +17,8 @@ import ru.z8.louttsev.cheaptripmobile.shared.model.data.*
 import ru.z8.louttsev.cheaptripmobile.shared.model.data.Route.Type
 import ru.z8.louttsev.cheaptripmobile.shared.infrastructure.persistence.LocalDbStorage
 import ru.z8.louttsev.cheaptripmobile.shared.infrastructure.persistence.RouteDb
+import ru.z8.louttsev.cheaptripmobile.shared.model.data.Route.Type.*
+import ru.z8.louttsev.cheaptripmobile.shared.model.data.TransportationType.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -22,72 +26,23 @@ import kotlin.test.assertTrue
 @Config(sdk = [24])
 @RunWith(AndroidJUnit4::class)
 class RouteRepositoryTest {
-    private val dataSourceFake = object : DataSource<Route> {
+    private val dataSourceWrapper = object : DataSource<Route> {
+        private val dataSource: FullDbDataSource<Route>
+
+        init {
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            val driver = DatabaseDriverFactory(context).getDriver(LocalDb.Schema, "fullDb.sqlite3")
+            dataSource = RouteDataSourceFullDb(driver)
+        }
+
         var isAvailable: Boolean = true
-        val routeStorage = emptyMap<Quadruple<Type, Location, Location, Locale>, Route>()
-            .toMutableMap()
-            .also {
-                it[Quadruple(
-                    Type.DIRECT,
-                    Location(1, "Moscow"),
-                    Location(2, "Petersburg"),
-                    Locale.EN
-                )] =
-                    Route(
-                        Type.DIRECT,
-                        35.3F,
-                        280,
-                        listOf(
-                            Path(
-                                TransportationType.TRAIN,
-                                35.3F,
-                                280,
-                                "Moscow",
-                                "Petersburg"
-                            )
-                        )
-                    )
-                it[Quadruple(
-                    Type.GROUND,
-                    Location(1, "Moscow"),
-                    Location(2, "Petersburg"),
-                    Locale.EN
-                )] =
-                    Route(
-                        Type.GROUND,
-                        48.5F,
-                        360,
-                        listOf(
-                            Path(
-                                TransportationType.BUS,
-                                28.5F,
-                                200,
-                                "Moscow",
-                                "Tver"
-                            ),
-                            Path(
-                                TransportationType.BUS,
-                                20.0F,
-                                160,
-                                "Tver",
-                                "Petersburg"
-                            )
-                        )
-                    )
-            }
 
         override fun get(parameters: ParamsBundle): List<Route>? {
-            if (!isAvailable) {
+            if (isAvailable) {
+                return dataSource.get(parameters)
+            } else {
                 return null
             }
-            val from = parameters.get(Key.FROM) as Location
-            val to = parameters.get(Key.TO) as Location
-            val locale = parameters.get(Key.LOCALE) as Locale
-            return routeStorage
-                .filterKeys {
-                    it.second == from && it.third == to && it.fourth == locale
-                }
-                .values.toList()
         }
     }
 
@@ -100,28 +55,29 @@ class RouteRepositoryTest {
     }
 
     private val repositoryUnderTest = RouteRepository(
-        dataSourceFake,
+        dataSourceWrapper,
         dataStorage,
         RepositoryStrategy.CACHING
     )
 
     @Test
     fun getRoutes() {
-        val fromLocation = Location(1, "Moscow")
-        val toLocation = Location(2, "Petersburg")
-        assertTrue(dataSourceFake.isAvailable)
+        val fromLocation = Location(387, "Moscow")
+        val toLocation = Location(9, "Tashkent")
+
+        val expectedResult = listOf(
+            Route(GROUND, 36.61F, 4020, listOf(Path(TRAIN, 36.61F, 4020, "Moscow", "Tashkent"))),
+            Route(FLYING, 151.256F, 916, listOf(Path(FLIGHT, 53.2F, 246, "Moscow", "Ashgabat"), Path(FLIGHT, 98.056F, 670, "Ashgabat", "Tashkent"))),
+            Route(DIRECT, 188.23F, 398, listOf(Path(FLIGHT, 188.23F, 398, "Moscow", "Tashkent")))
+        )
+
+        assertTrue(dataSourceWrapper.isAvailable)
         val result: List<Route> =
             repositoryUnderTest.getRoutes(fromLocation, toLocation, Locale.EN)
-        dataSourceFake.isAvailable = false
+        assertEquals(expectedResult, result)
+        dataSourceWrapper.isAvailable = false
         val cachedResult: List<Route> =
             repositoryUnderTest.getRoutes(fromLocation, toLocation, Locale.EN)
-        assertEquals(result, cachedResult)
+        assertEquals(expectedResult, cachedResult)
     }
 }
-
-data class Quadruple<out A, out B, out C, out D>(
-    val first: A,
-    val second: B,
-    val third: C,
-    val fourth: D
-)
