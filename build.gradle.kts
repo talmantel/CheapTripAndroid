7d99840fa705c3d93253d9a28144c90f4370c6cd
denis-luttcev <denis.luttcev@gmail.com>
2021-08-27 14:35:26 +0300

    Update dependencies versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("dev.icerock:mobile-multiplatform:0.12.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
         classpath("com.android.tools.build:gradle:4.2.2")
-        classpath("dev.icerock.moko:resources-generator:0.15.1")
+        classpath("dev.icerock.moko:resources-generator:0.17.0")
         classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
     }
 }
